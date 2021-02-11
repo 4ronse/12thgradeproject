2703019d0a217649d0a03544cd8968fafb94a0fc
Ron Segal <4ronse@gmail.com>
2021-02-11 17:03:47 +0200

    Added minimal argument support

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 """
 TODO: Implement my own Validator
 """
-
-
 import webserver
+import sys
 
 app = webserver.create_app()
 
 if __name__ == '__main__':
-    app.run(debug=webserver.Config.DEBUG)
+    debug = '-debug' in sys.argv or webserver.Config.DEBUG
+    ssl_context = None if '-nossl' in sys.argv or webserver.Config.NO_SSL else ('cret.crt, key.pem')
+
+    app.run(debug=debug, ssl_context=ssl_context)
