1d164393b4b62ebb096939e39323cbe83c053303
Ron Segal <4ronse@gmail.com>
2021-02-11 03:16:35 +0200

    Property password is now unreadable but setting it will automatically encrypt it via SHA-256

--- webserver/models/User.py
+++ webserver/models/User.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
 from .Base import BaseUserRelatedModel 
 from .. import db
 from ..config import Config
@@ -9,6 +10,14 @@ class User(UserMixin, BaseUserRelatedModel):
     name = db.Column(db.String, nullable=False)
     password = db.Column(db.String(64), nullable=False)
     profile_picture = db.Column(db.String, nullable=True, default=Config.DEFAULT_PROFILE_PICTURE)
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password = generate_password_hash(password=password, method='sha256')
 
     def __repr__(self):
         return f"User[ID: {self.id}; Name: {self.name}; E-Mail: {self.email}; PasswordHash: {self.password}]"
