31ed127f947a893a11ec7a6df17403a7f0fc1e6c
Ron Segal <4ronse@gmail.com>
2021-02-11 03:58:21 +0200

    Removed some unnecessary code from main.js

--- webserver/web/static/js/main.js
+++ webserver/web/static/js/main.js
@@ -1,30 +1,3 @@
-/* function validateName(name) {
-    const re = /^([a-zA-Z ]){2,30}$/;
-    return re.test(name);
-}
-
-function validateEmail(email) {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
-function validatePassword(password) {
-    return true;
-}
-
-function validateLoginForm() {
-    const inputEmail = document.getElementById("email").value;
-    return validateEmail(inputEmail);
-}
-
-function validateRegisterationForm() {
-    const inputFullName = document.getElementById("name").value;
-    const inputEmail = document.getElementById("email").value;
-    const inputPassword = document.getElementById("password").value;
-
-    return validateName(inputFullName) && validateEmail(inputEmail) && validatePassword(inputPassword);
-} */
-
 window.addEventListener('load', (e) => {
     const forms = document.querySelectorAll('.needs-validation');
 
